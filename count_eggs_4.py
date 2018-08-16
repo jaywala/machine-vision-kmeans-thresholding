25b5a04246afb39121aeaa66e57912016b7a737a
Jay Motwani <jymotwani@hotmail.com>
2018-08-16 15:32:33 +1000

    test

--- count_eggs_4.py
+++ count_eggs_4.py
@@ -7,7 +7,7 @@ from k_means_thresholding import k_means_thresholding
 from filtered_k_means_thresholding import filtered_k_means_thresholding
 
 
-def count_eggs_4(im1):
+def count_eggs_4(im1,min_comp_size):
     # mask = filtered_k_means_thresholding(im1)
     kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(8,8))
     im2 = cv2.morphologyEx(im1,cv2.MORPH_OPEN,kernel2)
@@ -18,12 +18,15 @@ def count_eggs_4(im1):
     label_mask = np.zeros_like(mask,dtype=np.uint8)
     neighbour = [(-1,0),(1,0),(0,-1),(0,1)]
     label_count = 1
+
+    # First Pass of finding Connected Components
     for x in range(mask.shape[1]):
             for y in range(mask.shape[0]):
                 if mask[y,x] != 0:
                     label_low =  label_count
                     found = False
                     for i in range(4):
+                        # Check if neighbour is within the 
                         if 0<=y+neighbour[i][0]<mask.shape[0] and 0<=x+neighbour[i][1]<mask.shape[1]:
                             if mask[y+neighbour[i][0],x+neighbour[i][1]] != 0 and label_mask[y+neighbour[i][0],x+neighbour[i][1]] != 0:
                                 if label_mask[y+neighbour[i][0],x+neighbour[i][1]] < label_low:
@@ -33,7 +36,10 @@ def count_eggs_4(im1):
                         label_count += 1
                         # print(label_count)
                     label_mask[y,x] = label_low
+    plt.matshow(label_mask)
+    plt.show()
 
+    # Second Pass for finding connected components
     for x in range(label_mask.shape[1]):
         for y in range(label_mask.shape[0]):
             if mask[y,x] != 0:
@@ -43,7 +49,22 @@ def count_eggs_4(im1):
                         if 0<label_mask[y+neighbour[i][0],x+neighbour[i][1]]<label_low:
                             label_low = label_mask[y+neighbour[i][0],x+neighbour[i][1]]
                 label_mask[y,x] = label_low
+
+    plt.matshow(label_mask)
+    plt.show()
     
+    # Count Pixel size of each component
+    size_count = np.zeros(label_count)
+    for x in range(label_mask.shape[1]):
+        for y in range(label_mask.shape[0]):
+            size_count[label_mask[y,x]] += 1
+
+    for x in range(label_mask.shape[1]):
+        for y in range(label_mask.shape[0]):
+            if (size_count[label_mask[y,x]] < min_comp_size):
+                label_mask[y,x] = 0
+                size_count[label_mask[y,x]] -= 1
+
     prev = 0
     count = 0
     for x in range(label_mask.shape[1]):
@@ -51,16 +72,17 @@ def count_eggs_4(im1):
             if label_mask[y,x]>prev:
                 prev = label_mask[y,x]
                 count += 1
-    print('count is {}'.format(count))
-    print('prev is {}'.format(prev))
 
+    print('For Testing prev count is {}'.format(prev))
+    print('Egg count is {}'.format(count))
     
 
+    # gray_image = np.array((label_mask != 0),dtype=np.uint8)
 
-    color_img = cv2.cvtColor(label_mask,cv2.COLOR_GRAY2RGB)
-    color_img[:,:,0] = (color_img[:,:,0]+8)*2
-    color_img[:,:,1] = color_img[:,:,1]*3
-    color_img[:,:,2] = color_img[:,:,2]*2
+    color_img = cv2.cvtColor(im1,cv2.COLOR_GRAY2RGB)
+    color_img[:,:,0] = color_img[:,:,0] - label_mask * 40
+    color_img[:,:,1] = color_img[:,:,1] - label_mask * 12
+    color_img[:,:,2] = color_img[:,:,2] - label_mask * 42
     cv2.imshow('colour image',color_img)
     cv2.waitKey(0)
     return color_img
@@ -74,7 +96,7 @@ def count_eggs_4(im1):
 if __name__ == '__main__':
     # Map command line arguments to function arguments.
     im1 = cv2.imread(sys.argv[1],0)
-    x = int(sys.argv[2])
+    size = int(sys.argv[2])
     outputfile = sys.argv[3]
-    im = count_eggs_4(im1)
+    im = count_eggs_4(im1,size)
     cv2.imwrite(outputfile,im)
--- filtered_k_means_thresholding.py
+++ filtered_k_means_thresholding.py
@@ -9,7 +9,7 @@ from k_means_thresholding import k_means_thresholding
 def filtered_k_means_thresholding(im1):
     mask = k_means_thresholding(im1)
     kernel = np.ones((5,5),np.uint8)
-    kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(8,8))
+    kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(10,10))
     # mask_open = cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernel)
     mask_open2 = cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernel2)
     kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(2,2))
@@ -32,4 +32,5 @@ if __name__ == '__main__':
     im1 = cv2.imread(sys.argv[1],0)
     outputfile = sys.argv[2]
     mask = filtered_k_means_thresholding(im1)
-    # cv2.imwrite(outputfile,mask)
+    cv2.imwrite(outputfile,mask)
+
